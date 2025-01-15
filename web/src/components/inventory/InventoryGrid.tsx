bf9ba359f4f4edfada796d441602601b1b495870
Kremu <90266455+Kr3mu@users.noreply.github.com>
2025-01-15 22:51:04 +0100

    main

---
+++ web/src/components/inventory/InventoryGrid.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Inventory } from '../../typings';
+import WeightBar from '../utils/WeightBar';
+import InventorySlot from './InventorySlot';
+import { getTotalWeight } from '../../helpers';
+import { useAppSelector } from '../../store';
+import { useIntersection } from '../../hooks/useIntersection';
+import { get } from 'lodash';
+
+const PAGE_SIZE = 30;
+
+const InventoryGrid: React.FC<{ inventory: Inventory }> = ({ inventory }) => {
+  const weight = useMemo(
+    () => (inventory.maxWeight !== undefined ? Math.floor(getTotalWeight(inventory.items) * 1000) / 1000 : 0),
+    [inventory.maxWeight, inventory.items]
+  );
+  const [page, setPage] = useState(0);
+  const containerRef = useRef(null);
+  const { ref, entry } = useIntersection({ threshold: 0.5 });
+  const isBusy = useAppSelector((state) => state.inventory.isBusy);
+
+  useEffect(() => {
+    if (entry && entry.isIntersecting) {
+      setPage((prev) => ++prev);
+    }
+  }, [entry]);
+
+
+  function getIcon(inventoryType: string) {
+      if (inventoryType == "player") {
+        return 'fa-solid fa-user';
+      } else if (inventoryType == "crafting") {
+        return 'fa-solid fa-wrench';
+      } else if (inventoryType == "shop") {
+        return 'fa-solid fa-store'
+      } 
+      return "fa-solid fa-cabinet-filing";
+  }
+
+  return (
+    <>
+      <div className="inventory-grid-wrapper" style={{ pointerEvents: isBusy ? 'none' : 'auto' }}>
+        <div>
+          <div className="inventory-grid-header-wrapper">
+            <div className="column name">
+              <div className="icon-background">
+
+                <i className={getIcon(inventory.type)}></i>
+              </div>
+              <p>{inventory.label}</p>
+            </div>
+            <div className="column">
+              {inventory.maxWeight && (
+                <p>
+                  {weight / 1000}/{inventory.maxWeight / 1000}kg
+                </p>
+              )}
+            </div>
+          </div>
+          <WeightBar percent={inventory.maxWeight ? (weight / inventory.maxWeight) * 100 : 0} />
+        </div>
+        <div className="inventory-grid-container" ref={containerRef}>
+          <>
+            {inventory.items.slice(0, (page + 1) * PAGE_SIZE).map((item, index) => (
+              <InventorySlot
+                key={`${inventory.type}-${inventory.id}-${item.slot}`}
+                item={item}
+                ref={index === (page + 1) * PAGE_SIZE - 1 ? ref : null}
+                inventoryType={inventory.type}
+                inventoryGroups={inventory.groups}
+                inventoryId={inventory.id}
+              />
+            ))}
+          </>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InventoryGrid;
